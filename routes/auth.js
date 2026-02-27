c0d187dadfa7367b9cfd3e8eb38ae91e97bb51ff
pyunqi <pyunqi@gmail.com>
2026-02-27 21:38:02 +0800

    replace bcrypt with bcryptjs for cross-platform compatibility

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const bcrypt  = require('bcrypt');
+const bcrypt  = require('bcryptjs');
 const router  = express.Router();
 
 // GET /admin/login
