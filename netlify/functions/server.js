17a7b8f1118a1fce6138dbbca728c16cad826746
pyunqi <pyunqi@gmail.com>
2026-02-27 21:47:00 +0800

    fix: explicitly require ejs so bundler includes it

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Explicitly require template engine so bundler includes it
+require('ejs');
+
 const serverless = require('serverless-http');
 const app        = require('../../server');
 
